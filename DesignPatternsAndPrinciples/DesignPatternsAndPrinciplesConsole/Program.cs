4f6a34107b3e98f42465c433c982e81f2e1fefd8
Mücahit imre <mucahit.imre@gmail.com>
2021-12-20 00:27:12 +0300

    created console project

---
+++ DesignPatternsAndPrinciples/DesignPatternsAndPrinciplesConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatternsAndPrinciplesConsole;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
