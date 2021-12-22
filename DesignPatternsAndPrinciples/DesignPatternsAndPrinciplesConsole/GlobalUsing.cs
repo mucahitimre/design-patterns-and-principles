5d5f67bb6e871aa30298eb86fb7248b4d120f8ce
Mücahit imre <mucahit.imre@gmail.com>
2021-12-22 07:09:26 +0300

    add new namespace

--- DesignPatternsAndPrinciples/DesignPatternsAndPrinciplesConsole/GlobalUsing.cs
+++ DesignPatternsAndPrinciples/DesignPatternsAndPrinciplesConsole/GlobalUsing.cs
@@ -1,1 +1,2 @@
-﻿global using System;
+﻿global using UnitOfWork;
+global using UnitOfWork.Abstractions;
--- DesignPatternsAndPrinciples/DesignPatternsAndPrinciplesConsole/Program.cs
+++ DesignPatternsAndPrinciples/DesignPatternsAndPrinciplesConsole/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using UnitOfWork;
 
 namespace DesignPatternsAndPrinciplesConsole;
 
