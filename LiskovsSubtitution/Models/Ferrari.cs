79d1e6feab49f233058c09c3270f89683f1f32e1
Mücahit imre <mucahit.imre@gmail.com>
2022-01-02 13:18:05 +0300

    Liskovs subtitution

---
+++ LiskovsSubtitution/Models/Ferrari.cs
@@ -0,0 +1,16 @@
+﻿using LiskovsSubtitution.Abstractions;
+
+namespace LiskovsSubtitution.Models;
+
+public class Ferrari : Car, IAirConditionable
+{
+    public void OpenAirConditioning()
+    {
+        Console.WriteLine($"{GetType().Name} air conditioner on..");
+    }
+
+    public override void Run()
+    {
+        Console.WriteLine($"{GetType().Name} is runuing..");
+    }
+}
--- LiskovsSubtitution/Program.cs
+++ LiskovsSubtitution/Program.cs
@@ -1,14 +1,35 @@
-﻿namespace LiskovsSubtitution
+﻿using LiskovsSubtitution.Abstractions;
+using LiskovsSubtitution.Models;
+
+namespace LiskovsSubtitution;
+
+public class Program
 {
-    public class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        /*
+         * Burada Car'dan türetilmiş her class'ın Car'ın taşıdığı özellikleri karşılayabilmesi gerekmektedir,
+            ve klimasız arabalarda olabileceğinden klima açma özelliğini Car'dan ayırarak 
+            Car'dan türeyen class'ların tüm özelliklerini gerçekleştirebimesini sağlamış olduk,
+            bu da bize Car'ın tüm özellikleri onu base alan class'lar tarafından gerçekleştirileceğini garanti etmiş oldu,
+            klima özelliğini'de ayrı bir interface ile yöneterek entegre etmiş olduk.
+         */
+
+        var ferrari = new Ferrari();
+        ferrari.Run();
+        if (ferrari is IAirConditionable airConditionableFerrari)
         {
-
-
-
-
-            Console.WriteLine("Hello World!");
+            airConditionableFerrari.OpenAirConditioning();
         }
+
+        var kartal = new Kartal();
+        kartal.Run();
+        if (kartal is IAirConditionable airConditionableKartal)
+        {
+            airConditionableKartal.OpenAirConditioning();
+        }
+
+        Console.WriteLine("Hello World!");
+        Console.ReadLine();
     }
 }
